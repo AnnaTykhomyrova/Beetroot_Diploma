41236e038dde4342b160eaf77d26e9ac94a10e59
AnnaTykhomyrova <tykhomyrovaanna@gmail.com>
2018-09-24 20:08:30 +0300

    without pixelperfect

--- script.js
+++ script.js
@@ -61,11 +61,17 @@ $(document).ready(function () {
         e.preventDefault();})
 
     // menu
-    (function($){
-        $(function() {
-            $('.menu__icon').on('click', function() {
-                $(this).closest('.menu').toggleClass('menu_state_open');
-            });
+    $(function() {
+        $('.menu__icon').on('click', function() {
+            $(this).closest('.menu')
+                .toggleClass('menu_state_open');
         });
-    })(jQuery);
+
+        $('.menu__links-item').on('click', function() {
+
+
+            $(this).closest('.menu')
+                .removeClass('menu_state_open');
+        });
+    });
 });
